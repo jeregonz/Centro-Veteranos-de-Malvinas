1afb70096ee4ead95eb3600e06d8b6057a1fe06d
Jeremias Gonzalez <jeregonza@hotmail.com.ar>
2024-11-17 19:41:29 -0300

    actualizacion de footer y ventana log in

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ document.addEventListener('DOMContentLoaded', function () {
             <div class="overlay">
                 <div class="popup">
                     <h2>Ingresar al sistema</h2>
-                    <input type="text" placeholder="Usuario" />
-                    <input type="password" placeholder="Contraseña" />
+                    
+                    <label for="username">Usuario</label>
+                    <input type="text" id="username"/>
+
+                    <label for="password">Contraseña</label>
+                    <input type="password" id="password"/>
                     <a href="#">¿Olvidó su contraseña?</a>
-                    <button id="closePopup">Ingresar</button>
+                    <button id="btn-log-in">Ingresar</button>
                 </div>
             </div>
         `;
@@ -16,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.insertAdjacentHTML('beforeend', popupHTML);
 
         // Manejar el cierre del popup
-        document.getElementById('closePopup').addEventListener('click', function () {
+        document.getElementById('btn-log-in').addEventListener('click', function () {
             document.querySelector('.overlay').remove();
         });
     });
